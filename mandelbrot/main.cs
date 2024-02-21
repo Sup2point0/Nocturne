06230036b25f65fe94c99d013dae90ad618fd68c
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-02-21 20:58:54 +0000

    restructure mandelbrot

--- mandelbrot/main.cs
+++ mandelbrot/main.cs
@@ -1,51 +1,19 @@
 using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-// using System.Drawing.Common;
 
 
 class Program
-{
-  public const double PlaneX = 1000d;
-  public const double PlaneY = 1000d;
-  public const double RenderStep = 1d;
-  
+{ 
   public static void Main()
   {
-    Console.WriteLine("STATUS: RUNNING!");
+    Log("RUNNING!");
+    Mandelbrot.Render(500);
+    // Mandelbrot.Render(500, (-2.0, 2.0), (-2.0, 2.0));
+    Log("FINISHED!");
+  }
 
-    Bitmap plane = new Bitmap((int)PlaneX, (int)PlaneY);
-    Graphics graphics = Graphics.FromImage(plane);
-
-    for (double x = 0; x < PlaneX; x += RenderStep)
-    {
-      for (double y = 0; y < PlaneY; y += RenderStep)
-      {
-        Complex z = new Complex(
-          4 * x / PlaneX - 2,
-          4 * y / PlaneY - 2
-        );
-        int iterations = z.Diverge().Count - 1;
-        
-        double frac;
-        frac = (double)iterations / (double)Complex.DivergeIterations;
-        frac = Math.Sqrt(frac);
-        Color colour = Colourise.Apply(frac, "inferno");
-
-        plane.SetPixel((int)x, (int)y, colour);
-
-        if (x == PlaneX/2 && y == PlaneY/2) {
-          Console.WriteLine("STATUS: HALFWAY...");
-        }
-      }
-    }
-
-    plane.Save("render.png", ImageFormat.Png);
-
-    graphics.Dispose();
-    plane.Dispose();
-
-    Console.WriteLine("STATUS: FINISHED!");
+  public static void Log(string text)
+  {
+    Console.WriteLine($"STATUS: {text}");
   }
 
 }
---
+++ mandelbrot/mandelbrot.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+public static class Mandelbrot
+{
+  public static void Render(
+    int size,
+    (double start, double stop) boundsX,
+    (double start, double stop) boundsY
+  ) {
+    Bitmap plane = new Bitmap(size, size);
+    Graphics graphics = Graphics.FromImage(plane);
+
+    for (int x = 0; x < size; x++)
+    {
+      for (int y = 0; y < size; y++)
+      {
+        Complex z = new Complex(
+          boundsX.start + (boundsX.stop - boundsX.start) * (double)x / (double)size,
+          boundsY.start + (boundsY.stop - boundsY.start) * (double)y / (double)size
+        );
+        int iterations = z.Diverge().Count - 1;
+        
+        double frac;
+        frac = (double)iterations / (double)Complex.DivergeIterations;
+        frac = Math.Sqrt(frac);
+        Color colour = Colourise.Apply(frac, "inferno");
+
+        plane.SetPixel((int)x, (int)y, colour);
+      }
+    }
+
+    plane.Save(
+      $"""C:\Users\Han\Desktop\大维\Stuff\Documents\# Code\C#\Mandelbrot\renders\render ({size}x{size}).png""",
+      ImageFormat.Png
+    );
+
+    graphics.Dispose();
+    plane.Dispose();
+  }
+
+  public static void Render(int size)
+  {
+    Render(size, (-2.0, 2.0), (-2.0, 2.0));
+  }
+}
